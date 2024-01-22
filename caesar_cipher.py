27a391de253d6cb954852175a33a346825b3d74d
Oğuz Kayra Büyükyıldırım <74321096+OguzKaira@users.noreply.github.com>
2024-01-23 00:38:39 +0300

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,24 +1,35 @@
+import getpass
+
 def caesar_cipher(text, shift, direction):
     result = ""
     for char in text:
         if char.isalpha():
-            if direction == 'e':
-                result += chr((ord(char) + shift - ord('a')) % 26 + ord('a')) if char.islower() else chr((ord(char) + shift - ord('A')) % 26 + ord('A'))
-            elif direction == 'd':
-                result += chr((ord(char) - shift - ord('a')) % 26 + ord('a')) if char.islower() else chr((ord(char) - shift - ord('A')) % 26 + ord('A'))
+            base = ord('a') if char.islower() else ord('A')
+            result += chr((ord(char) + shift - base) % 26 + base) if direction == 'e' else chr((ord(char) - shift - base) % 26 + base)
         else:
             result += char
     return result
 
+def main():
+    print("Welcome to the Caesar Cipher Encryption/Decryption tool!")
+    text = getpass.getpass("Enter the text to be encrypted/decrypted: ")
+
+    while True:
+        direction = input("Do you want to encrypt or decrypt? (e/d): ").lower()
+        if direction in ['e', 'd']:
+            break
+        else:
+            print("Invalid input. Please enter 'e' for encryption or 'd' for decryption.")
+
+    while True:
+        try:
+            shift = int(input("Enter the shift value for the Caesar cipher: "))
+            break
+        except:
+            print("Problem occurred!")
+
+    result = caesar_cipher(text, shift, direction)
+    print("Result: {}".format(result))
+
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(description="Caesar Cipher Encryption/Decryption")
-    parser.add_argument("-e", "--encrypt", action="store_true", help="Encrypt the text")
-    parser.add_argument("-d", "--decrypt", action="store_true", help="Decrypt the text")
-
-    args = parser.parse_args()
-  
-    direction = 'e' if args.encrypt else 'd'
-    result = caesar_cipher(args.text, args.shift, direction)
-    print(f"Result: {result}")
+    main()
